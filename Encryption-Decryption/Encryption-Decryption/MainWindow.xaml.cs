e4d261eeec2d78155b36a7ce657a48692f0ed2d3
Emre <72259197+4Forcas@users.noreply.github.com>
2022-03-17 18:54:59 +0100

    Boiler UI

--- Encryption-Decryption/Encryption-Decryption/MainWindow.xaml.cs
+++ Encryption-Decryption/Encryption-Decryption/MainWindow.xaml.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using EncryptionDecryption.Pages;
 
-namespace Encryption_Decryption
+namespace EncryptionDecryption
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decrypt_Clicked(object sender, RoutedEventArgs e) => frMain.Content = new Decrypt();
+        private void Encrypt_Clicked(object sender, RoutedEventArgs e) => frMain.Content = new Encrypt();
+        private void BtnClose_Click(object sender, RoutedEventArgs e) => this.Close();
+        private void Btnminimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => this.DragMove();
     }
 }
