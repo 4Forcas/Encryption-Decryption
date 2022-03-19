fc74fe1f305f8abfbde441a68254e553305e591d
Emre <72259197+4Forcas@users.noreply.github.com>
2022-03-19 14:22:36 +0100

    Generate publicKey and privateKey

--- Encryption-Decryption/Encryption-Decryption/Pages/Encrypt.xaml.cs
+++ Encryption-Decryption/Encryption-Decryption/Pages/Encrypt.xaml.cs
@@ -1,25 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace EncryptionDecryption.Pages
 {
-    /// <summary>
-    /// Interaction logic for Encrypt.xaml
-    /// </summary>
     public partial class Encrypt : Page
     {
         public Encrypt()
         {
             InitializeComponent();
+        }
+
+        void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.CheckFileExists = false;
+            ofd.FileName = "Location";
+            if (ofd.ShowDialog() == true)
+            {
+                string folderPath = Path.GetDirectoryName(ofd.FileName);
+                string publicKeyPath = Path.Combine(folderPath, "publicKey.xml");
+                string privateKeyPath = Path.Combine(folderPath, "privateKey.xml");
+                string publickey;
+                string privatekey;
+                GenerateRSAKey(out publickey, out privatekey);
+                using (var sw = File.CreateText(publicKeyPath))
+                {
+                    sw.Write(publickey);
+                }
+                using (var sw = File.CreateText(privateKeyPath))
+                {
+                    sw.Write(privatekey);
+                }
+            }
+        }
+
+
+        public void GenerateRSAKey(out string publickey, out string privatekey)
+        {
+            var rsa = new RSACryptoServiceProvider();
+            publickey = rsa.ToXmlString(false);
+            privatekey = rsa.ToXmlString(true);
+        }
+
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                InitialDirectory = @"C:\",
+            };
+            if (ofd.ShowDialog() == true)
+            {
+                
+
+            }
+
+        }
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
