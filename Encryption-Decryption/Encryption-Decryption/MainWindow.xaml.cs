262292d0d7bfe86dbfac1fe5b01f1a71379fc05e
JarneStaalPXL <jarne.staal9@outlook.com>
2022-04-18 11:19:15 +0200

    UI update

--- Encryption-Decryption/Encryption-Decryption/MainWindow.xaml.cs
+++ Encryption-Decryption/Encryption-Decryption/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Windows;
+﻿using EncryptionDecryption.Pages;
+using System.Windows;
 using System.Windows.Input;
-using EncryptionDecryption.Pages;
 
 namespace EncryptionDecryption
 {
@@ -22,6 +22,8 @@ namespace EncryptionDecryption
 
         private void Decrypt_Clicked(object sender, RoutedEventArgs e) => frMain.Content = new Decrypt();
         private void Encrypt_Clicked(object sender, RoutedEventArgs e) => frMain.Content = new Encrypt();
+        private void EncryptAES_Clicked(object sender, RoutedEventArgs e) => frMain.Content = new EncryptAES();
+        private void DecryptAES_Clicked(object sender, RoutedEventArgs e) => frMain.Content = new DecryptAES();
         private void BtnClose_Click(object sender, RoutedEventArgs e) => this.Close();
         private void Btnminimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
         private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => this.DragMove();
